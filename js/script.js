8a7d287f6790ed685d88fea61a43d06bf2b93cc5
Kevin <kvalenzuela12345@gmail.com>
2019-01-02 22:44:41 +0000

    buzzfeed

--- js/script.js
+++ js/script.js
@@ -8,7 +8,43 @@ $(document).ready(function() {
         var q1Input = $("#question1").val();
         var q2Input = $("#question2").val();
         var q3Input = $("#question3").val();
-        var totalScore;
+        var totalScore = question1Score(q1Input) + question2Score(q2Input) + question3Score(q3Input);
+        var score1 = question1Score(q1Input);
+        
+        
+        
+        if(q2Input == "Google"){
+            userScore + 9
+        }else{
+            userScore +8
+        }
+        
     });
 
+function question1Score(q1Input){
+        if (q1Input == "coding" || q1Input == "reading") {
+             return 9;
+         }else{
+        return -2;
+        }
+}
+
+function question2Score(q2Input){
+        if (q1Input == "coding" || q2Input == "reading") {
+             return 9;
+         }else{
+        return -2;
+        }
+}
+
+function question3Score(q3Input){
+        if (q1Input == "coding" || q3Input == "reading") {
+             return 9;
+         }else{
+        return -2;
+        }
+}
+        
 });
+   
+   
